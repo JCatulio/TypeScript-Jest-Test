3ba00aa5f90096fa4256570083c8e3e28307b95b
Jonathan <82682806+JCatulio@users.noreply.github.com>
2022-04-12 13:48:02 -0300

    tj

--- src/services/persistency.spec.ts
+++ src/services/persistency.spec.ts
@@ -1,13 +1,13 @@
 describe('TESTANDO ALGUMA COISA', () => {
   it('should return one', () => {
     const number = 1;
-    expect(number).not.toBe(1);
+    expect(number).toBe(1);
   });
 });
 
 describe('TESTANDO OUTRA COISA', () => {
   test('should return Luiz', () => {
     const nome = 'Luiz';
-    expect(nome).not.toBe('Luiz');
+    expect(nome).toBe('Luiz');
   });
 });
