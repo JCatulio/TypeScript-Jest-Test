e9519b285b438f5b1b8b5c479c5c18d410bd87e2
Jonathan <82682806+JCatulio@users.noreply.github.com>
2022-04-12 12:21:19 -0300

    first commit

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    semi: true, // ponto e virgula
+    trailingComma: 'all',
+    singleQuote: true,
+    printWidth: 80,
+    tabWidth: 2,
+}
---
+++ src/classes/interfaces/order-status.ts
@@ -0,0 +1,1 @@
+export type OderStatus = 'open' | 'closed';
